81783c3746e7c10c2479ba4b02c7143c05636255
Nada Alsogour <99046134+Nada-bit73@users.noreply.github.com>
2022-05-29 18:25:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+var a = 0;
+function buttonClick1() {
+
+    document.getElementById('inc1').value = ++a;
+}
+
+var b = 0;
+function buttonClick2() {
+
+    document.getElementById('inc2').value = ++b;
+}
+
+var c = 0;
+function buttonClick3() {
+
+    document.getElementById('inc3').value = ++c;
+}
